8bd079d3e4f0313c709d22c604e0cf7bbcad8a75
Rafael Taboada <rafael@taboada.com.br>
2020-09-26 08:53:26 -0300

    Finalizada a montagem do banco de dados

--- class/Marcas.php
+++ class/Marcas.php
@@ -1,7 +1,10 @@
 ﻿<?php
 
-class Marcas{
+date_default_timezone_set("America/Sao_Paulo");
 
+class Marcas{
+    //ATRIBUTOS
+    private $id_marca;
 	private $id_master;
     private $id_oper;
     private $cd_marca;
@@ -10,7 +13,7 @@ class Marcas{
     private $dt_cad_marca;
     private $dt_alt_marca;
     private $es_inat_marca;
-
+    //MÉTODOS GET E SET
     public function getIdMaster(){
     	return $this->id_master;
     }
@@ -73,35 +76,192 @@ class Marcas{
     public function setEsInatMarca($value){
     	$this->es_inat_marca = $value;
     }
-
-    public function buscaId($id){
+    //MÉTODO DE BUSCAR ID
+    public function buscaId($id_marca){
 
     	$sql = new Sql();
 
     	$results = $sql->select("SELECT * FROM tb_marcas WHERE id_marca = :ID_MARCA", array(
-    	":ID_MARCA"=>$id
+    	":ID_MARCA"=>$id_marca
 		));
     
     	if(count($results) > 0){
 
-    		$row = $results[0];
+            $this->setData($results[0]);        
 
-    		$this->setIdMaster($row['id_master']);
-    		$this->setIdOper($row['id_oper']);
-    		$this->setIdMarca($row['id_marca']);
-    		$this->setCdMarca($row['cd_marca']);
-    		$this->setDsMarca($row['ds_marca']);
-    		$this->setDsObsMarca($row['ds_obs_marca']);
-    		$this->setDtCadMarca(new DateTime($row['dt_cad_marca']));
-    		$this->setDtAltMarca(new DateTime($row['dt_alt_marca']));
-    		//$this->setDtCadMarca($row['dt_cad_marca']);
-    		//$this->setDtAltMarca($row['dt_alt_marca']);
-    		$this->setEsInatMarca($row['es_inat_marca']);
-
-    	}
+        }
 
     }
+    //MÉTODO DE LISTAR CADASTROS
+    public static function listarMarcas(){
 
+    	$sql = new Sql();
+    	return $sql->select("SELECT * FROM tb_marcas order by ds_marca");
+
+    }
+    //MÉTODO DE BUSCAR POR DESCRIÇÃO
+    public static function buscaDesc($desc){
+
+    	$sql = new Sql();
+    	return $sql->select("SELECT * FROM tb_marcas WHERE ds_marca LIKE :busca ORDER BY ds_marca", array(
+    		":busca"=>"%" . $desc . "%"
+    	));
+
+    }
+    //MÉTODO DE BUSCAR ATRAVÉS DE UM ID E SENHA
+    public function buscaIDmarcaEmaster($id_marca,$id_master){
+
+		$sql = new Sql();
+
+    	$results = $sql->select("SELECT * FROM tb_marcas WHERE id_marca = :ID_MARCA AND id_master = :ID_MASTER", array(
+    	":ID_MARCA"=>$id_marca,
+    	":ID_MASTER"=>$id_master
+		));
+    
+    	if(count($results) > 0){
+
+            $this->setData($results[0]);    	
+
+    	}else{
+
+    		throw new Exception("Algum ID está errado");
+
+    	}
+     }
+    //MÉTODO PARA UTILIZAR O SET DO BANCO DE DADOS
+    public function setData($data){
+
+        $this->setIdMaster($data["id_master"]);
+        $this->setIdOper($data["id_oper"]);
+        $this->setIdMarca($data["id_marca"]);
+        $this->setCdMarca($data["cd_marca"]);
+        $this->setDsMarca($data["ds_marca"]);
+        $this->setDsObsMarca($data["ds_obs_marca"]);
+        $this->setEsInatMarca($data["es_inat_marca"]);
+        $this->setDtCadMarca(new DateTime($data["dt_cad_marca"]));
+        $this->setDtAltMarca(new DateTime($data["dt_alt_marca"]));
+        //$this->setDtCadMarca($data["dt_cad_marca"]);
+        //$this->setDtAltMarca($data["dt_alt_marca"]);
+
+    }
+    //ABAIXO É O FORMATO DE INSERIR NO BANCO ATRAVÉS DE PROCEDURE, PORÉM O PHPMYADMIN NÃO ESTÁ APTO A PROCEDURES (25/09/2020)
+    /*public function inserirMarcaProcedure(){
+           $sql = new Sql();
+           $results = $sql->select("CALL sp_marcas_insert(:ID_MASTER, :ID_OPER, :ID_MARCA, :CD_MARCA, :DS_MARCA, :DS_OBS_MARCA, :DT_CAD_MARCA, :DT_ALT_MARCA,:ES_INAT_MARCA)", array(
+           ":ID_MASTER"=>$this->getIdMaster(),
+    		":ID_OPER"=>$this->getIdOper(),
+    		":ID_MARCA"=>$this->getIdMarca(),
+    		":CD_MARCA"=>$this->getCdMarca(),
+    		":DS_MARCA"=>$this->getDsMarca(),
+    		":DS_OBS_MARCA"=>$this->getDsObsMarca(),
+    		":ES_INAT_MARCA"=>$this->getEsInatMarca(),
+    		":DT_CAD_MARCA"=>$this->getDtCadMarca(),
+    		":DT_ALT_MARCA"=>$this->getDtAltMarca()
+           ));
+           if(count($results) > 0){
+            $this->setData($results[0]);        
+            }
+    }*/
+    
+    //MÉTODO PARA INSERIR DADOS NO BANCO
+    
+    public function inserirMarca($id_master, $id_oper,$id_marca, $cd_marca, $ds_marca, $ds_obs_marca, $es_inat_marca, $dt_cad_marca, $dt_alt_marca){
+
+        $this->setIdMaster($id_master);
+        $this->setIdOper($id_oper);
+        $this->setIdMarca($id_marca);
+        $this->setCdMarca($cd_marca);
+        $this->setDsMarca($ds_marca);
+        $this->setDsObsMarca($ds_obs_marca);
+        $this->setEsInatMarca($es_inat_marca);
+        //$this->setDtCadMarca(new DateTime($dt_cad_marca));
+        //$this->setDtAltMarca(new DateTime($dt_alt_marca));
+        $this->setDtCadMarca($dt_cad_marca);
+        $this->setDtAltMarca($dt_alt_marca);
+
+        $sql = new Sql();
+
+        $sql->query("INSERT INTO tb_marcas (id_master, id_oper, id_marca, cd_marca, ds_marca, ds_obs_marca, es_inat_marca, dt_cad_marca, dt_alt_marca)
+                            VALUES (:ID_MASTER, :ID_OPER, :ID_MARCA, :CD_MARCA, :DS_MARCA, :DS_OBS_MARCA, :ES_INAT_MARCA, :DT_CAD_MARCA, :DT_ALT_MARCA)", array(
+            ":ID_MASTER"=>$this->getIdMaster(),
+            ":ID_OPER"=>$this->getIdOper(),
+            ":ID_MARCA"=>$this->getIdMarca(),
+            ":CD_MARCA"=>$this->getCdMarca(),
+            ":DS_MARCA"=>$this->getDsMarca(),
+            ":DS_OBS_MARCA"=>$this->getDsObsMarca(),
+            ":ES_INAT_MARCA"=>$this->getEsInatMarca(),
+            ":DT_CAD_MARCA"=>$this->getDtCadMarca(),
+            ":DT_ALT_MARCA"=>$this->getDtAltMarca()
+            ));
+    }
+    
+    //MÉTODO PARA ATUALIZAR/EDITAR DADOS DO BANCO
+
+    public function update($id_master, $id_oper, $cd_marca, $ds_marca, $ds_obs_marca, $es_inat_marca, $dt_cad_marca, $dt_alt_marca){
+
+        $this->setIdMaster($id_master);
+        $this->setIdOper($id_oper);
+        $this->setCdMarca($cd_marca);
+        $this->setDsMarca($ds_marca);
+        $this->setDsObsMarca($ds_obs_marca);
+        $this->setEsInatMarca($es_inat_marca);
+        //$this->setDtCadMarca(new DateTime($dt_cad_marca));
+        //$this->setDtAltMarca(new DateTime($dt_alt_marca));
+        $this->setDtCadMarca($dt_cad_marca);
+        $this->setDtAltMarca($dt_alt_marca);
+
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_marcas SET id_master = :ID_MASTER, id_oper = :ID_OPER, cd_marca = :CD_MARCA, ds_marca = :DS_MARCA,
+        ds_obs_marca = :DS_OBS_MARCA, es_inat_marca = :ES_INAT_MARCA, dt_cad_marca = :DT_CAD_MARCA, dt_alt_marca = :DT_ALT_MARCA WHERE id_marca = :ID_MARCA", array(
+            ":ID_MASTER"=>$this->getIdMaster(),
+            ":ID_OPER"=>$this->getIdOper(),
+            ":CD_MARCA"=>$this->getCdMarca(),
+            ":DS_MARCA"=>$this->getDsMarca(),
+            ":DS_OBS_MARCA"=>$this->getDsObsMarca(),
+            ":ES_INAT_MARCA"=>$this->getEsInatMarca(),
+            ":DT_CAD_MARCA"=>$this->getDtCadMarca(),
+            ":DT_ALT_MARCA"=>$this->getDtAltMarca(),
+            ":ID_MARCA"=>$this->getIdMarca()
+            ));
+    }
+
+    //MÉTODO PARA DELETAR DADOS DO BANCO
+    public function delete(){
+
+        $sql = new Sql();
+        $sql->query("DELETE FROM tb_marcas WHERE id_marca = :ID_MARCA", array(
+            ":ID_MARCA"=>$this->getIdMarca()
+             ));
+        $this->setIdMaster("");
+        $this->setIdOper("");
+        $this->setIdMarca("");
+        $this->setCdMarca("");
+        $this->setDsMarca("");
+        $this->setDsObsMarca("");
+        $this->setEsInatMarca("");
+        $this->setDtCadMarca(new DateTime());
+        $this->setDtAltMarca(new DateTime());                
+    }
+
+    //MÉTODO CONSTRUTOR PARA QUE OS DADOS SEJAM PASSADOS DENTRO DA FUNÇÃO CHAMADA
+    public function __construct($id_master = "", $id_oper = "", $id_marca = "", $cd_marca = "", $ds_marca = "", $ds_obs_marca = "",
+    $es_inat_marca = "", $dt_cad_mara = "",$dt_alt_marca = ""){
+
+        $this->setIdMaster($id_master);
+        $this->setIdOper($id_oper);
+        $this->setIdMarca($id_marca);
+        $this->setCdMarca($cd_marca);
+        $this->setDsMarca($ds_marca);
+        $this->setDsObsMarca($ds_obs_marca);
+        $this->setEsInatMarca($es_inat_marca);
+        $this->setDtCadMarca($dt_cad_marca);
+        $this->setDtAltMarca($dt_alt_marca);
+        //$this->setDtCadMarca(new DateTime($dt_cad_marca));
+        //$this->setDtAltMarca(new DateTime($dt_alt_marca));
+
+    }
+    //MÉTODO PARA TRANSFORMAR AS INFORMAÇÕES EM JSON_ENCODE FACILITANDO A MANIPULAÇÃO E A VISUALIZAÇÃO NO SITE
     public function __toString(){
 
     	return json_encode(array(
@@ -111,11 +271,11 @@ class Marcas{
     		"cd_marca"=>$this->getCdMarca(),
     		"ds_marca"=>$this->getDsMarca(),
     		"ds_obs_marca"=>$this->getDsObsMarca(),
-    		"dt_cad_marca"=>$this->getDtCadMarca()->format("d/m/Y H:i:s"),
-    		"dt_alt_marca"=>$this->getDtAltMarca()->format("d/m/Y H:i:s"),
-    		//"dt_cad_marca"=>$this->getDtCadMarca(),
-    		//"dt_alt_marca"=>$this->getDtAltMarca(),
-    		"es_inat_marca"=>$this->getEsInatMarca()
+    		"es_inat_marca"=>$this->getEsInatMarca(),
+    		//"dt_cad_marca"=>$this->getDtCadMarca()->format("d/m/Y H:i:s"),
+    		"dt_cad_marca"=>$this->getDtCadMarca(),
+    		//"dt_alt_marca"=>$this->getDtAltMarca()->format("d/m/Y H:i:s")
+    		"dt_alt_marca"=>$this->getDtAltMarca()
     	));
 
     }
--- index.php
+++ index.php
@@ -7,13 +7,59 @@ require_once("config.php");
 //$marcas = $sql->select("SELECT * FROM tb_marcas");
 //echo json_encode($marcas);
 
-//Utilizando a classe Marcas
-$busca_marca = new Marcas();
-
+//Utilizando a classe Marcas para chamar um registro
+/*$busca_marca = new Marcas();
 $marca = "62";
-
 $busca_marca->buscaId($marca);
+echo $busca_marca;*/
 
-echo $busca_marca;
+//Utilizando a classe Marcas para chamar todos os registros
+//$marcas = Marcas::listarMarcas();
+//echo json_encode($marcas);
 
+//Utilizando a classe Marcas para fazer uma busca por descrição
+/*$busca = "tes";
+$marcas = Marcas::buscaDesc($busca);
+echo json_encode($marcas);*/
+
+//Utilizando a classe Marcas para fazer uma busca por IDs como um login e senha
+/*$busca = new Marcas();
+$id_master = "0001";
+$id_marca = "000063";
+$busca->buscaIDmarcaEmaster($id_marca, $id_master);
+echo $busca;*/
+
+//Utilizando a classe Marcas para inserir dados no banco com o comando procedure(POR? O PHPMYADMIN N? EST? APTO A PROCEDURES (25/09/2020))
+/*$marca = new Marcas();
+$marca->setIdMaster("0050");
+$marca->setIdOper("005050");
+$marca->setIdMarca("005151");
+$marca->setCdMarca("005252");
+$marca->setDsMarca("Teste Dezena 50");
+$marca->setDsObsMarca("Deu certo 50 vezes");
+$marca->setDtCadMarca("2018-06-05 22:58:03");
+$marca->setDtAltMarca("2018-06-05 22:58:03");
+$marca->setEsInatMarca("A");
+$marca->inserirMarcaProcedure();
+echo $marca;*/
+
+//Utilizando a classe Marcas para inserir dados no banco
+
+/*$marcas = new Marcas();
+$marcas->inserirMarca('0091','009091','000003','009191','Teste Inserir 91','Deu certo 91 vezes','I','2020-09-25 22:58:03','2020-09-25 22:58:03');
+echo $marcas;*/
+
+//Utilizando a classe Marcas para atualizar dados do Banco
+/*$marcas = new Marcas();
+$id_marca = "000054";
+$marcas->buscaId($id_marca);
+$marcas->update('0050','005050','005252','Teste Update','Deu certo 70 vezes','I','2018-06-12 22:58:03','2018-06-12 22:58:03');
+echo $marcas;*/
+
+//Utilizando a classe Marcas para deletar dados do banco
+$marcas = new Marcas();
+$id_marca = "000057";
+$marcas->buscaId($id_marca);
+$marcas->delete();
+echo $marcas;
 ?>
